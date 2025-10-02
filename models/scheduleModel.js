d9e1b1a4b65f7a7a8246b68e921be4c1f7986ad7
CordovaPaulo <202311378@gordoncollege.edu.ph>
2025-10-02 11:47:23 +0800

    schedule schema

--- models/scheduleModel.js
+++ models/scheduleModel.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const scheduleSchema = new mongoose.Schema({
     zone: { type: String, required: true },
     barangay: { type: String, required: true },
-    day: { type: String, required: true },
-    type: { type: String, required: true },
+    types: [
+        {
+            typeName: { type: String, required: true },
+            day: { type: String, required: true }
+        }
+    ]
 }, { collection: 'schedules' });
 
 const Schedule = mongoose.model('Schedule', scheduleSchema);
